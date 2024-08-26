0eac2c984505885bd5a8207d2678628972ebf15a
Lana <lana@lana.com>
2024-08-26 02:16:40 -0400

    trabajo que cosnta de evaluar la te

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int temperatura = 0;
+        int temperatura;
         int option = 0;
         int money = 10000;
 
